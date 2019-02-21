001367c6b45331fe23b20490224c1ba00e73c677
Aleksandr Belets <askii.robotics@gmail.com>
2019-02-22 00:31:30 +0300

    code style correction for root component

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,7 @@
  */
 'use strict';
 
-import React, { Component } from 'react';
-import {
-  Platform, Text, View,
-} from 'react-native';
+import React from 'react';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/lib/integration/react';
 import { persistor, store } from './store';
