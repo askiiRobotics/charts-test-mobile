4370272706fc763a61253508d541cec5ec971820
Aleksandr Belets <askii.robotics@gmail.com>
2019-02-21 22:27:58 +0300

    corrected files according to ESLint rules

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
+ * Root component for the Charts Application
  *
  * @format
  * @flow
  * @lint-ignore-every XPLATJSCOPYRIGHT1
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from 'react';
+import {
+  Platform, StyleSheet, Text, View,
+} from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+    'Double tap R on your keyboard to reload,\n'
+    + 'Shake or press menu button for dev menu',
 });
 
 type Props = {};
