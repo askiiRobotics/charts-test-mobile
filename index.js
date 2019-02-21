d164db7eda0e3924b3c95baf549132963336f3aa
Aleksandr Belets <askii.robotics@gmail.com>
2019-02-21 23:38:23 +0300

    updated root components

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /**
  * @format
- * @lint-ignore-every XPLATJSCOPYRIGHT1
  */
+'use strict';
 
 import { AppRegistry } from 'react-native';
 import App from './src/App';
--- src/App.jsx
+++ src/App.jsx
@@ -3,49 +3,26 @@
  *
  * @format
  * @flow
- * @lint-ignore-every XPLATJSCOPYRIGHT1
  */
+'use strict';
 
 import React, { Component } from 'react';
 import {
-  Platform, StyleSheet, Text, View,
+  Platform, Text, View,
 } from 'react-native';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/lib/integration/react';
+import { persistor, store } from './store';
+import { RootComponent, LoadingView } from './components';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n'
-    + 'Shake or press menu button for dev menu',
-});
+const App = () => {
+  return (
+    <Provider store={store}>
+      <PersistGate loading={<LoadingView />} persistor={persistor}>
+        <RootComponent />
+      </PersistGate>
+    </Provider>
+  );
+};
 
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+export default App;
