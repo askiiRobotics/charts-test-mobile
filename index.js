9708fd5374d01b635284e2751a8b75826ac802e9
Aleksandr Belets <askii.robotics@gmail.com>
2019-02-22 23:29:49 +0300

    added config and updated root files

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 'use strict';
 
 import { AppRegistry } from 'react-native';
-import App from './src/App';
-import { name as appName } from './app.json';
+import App from './src';
+import { name as appName } from './app';
 
 AppRegistry.registerComponent(appName, () => App);
---
+++ src/index.js
@@ -0,0 +1,12 @@
+/**
+ * all exports of store folder
+ */
+
+import App from './App';
+// it a future it should be replaced with react-native-config for better build automatization
+import config from '../appconfig'; 
+
+export {
+    config
+};
+export default App;
