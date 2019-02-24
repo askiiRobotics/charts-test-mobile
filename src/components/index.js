e2a31efb36cf49ff398d151a44f849c728d9790a
Aleksandr Belets <askii.robotics@gmail.com>
2019-02-24 22:29:44 +0300

    commented planned component for velocity indication

--- src/components/index.js
+++ src/components/index.js
@@ -4,7 +4,8 @@
 
 export { default as ChartColumn } from './ChartColumn';
 export { default as ChartDashboard } from './ChartDashboard';
-export { default as ChartDynamicIndication } from './ChartDynamicIndication';
+// TODO: implement
+// export { default as ChartDynamicIndication } from './ChartDynamicIndication';
 export { default as ChartGrid } from './ChartGrid';
 export { default as LoadingView } from './LoadingView';
 export { default as RootComponent } from './RootComponent';
