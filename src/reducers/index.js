29deccaa400515ff6320a63fbefb82501b938587
Aleksandr Belets <askii.robotics@gmail.com>
2019-02-24 21:03:43 +0300

    fix import order

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,5 @@
 /**
  * all exports of reducers folder
  */
-import reducers from './reducers';
-
 export * from './userFinancialInfo';
-export { default as userFinancialInfo } from './userFinancialInfo';
-export default reducers;
+export { default as reducers } from './reducers';
--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,6 @@
  * all exports of store folder
  */
 
-export * from './store';
 export { default as array } from './array';
 export { default as promise } from './promise';
+export * from './store';
