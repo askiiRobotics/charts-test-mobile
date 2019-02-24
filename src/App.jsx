f40f5e4285e7d20c527cf05f46b6c7720e70864f
Aleksandr Belets <askii.robotics@gmail.com>
2019-02-24 21:05:50 +0300

    hided yellow boxes in app

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,20 @@
  * @format
  * @flow
  */
+/* global __DEV__ */
 'use strict';
 
 import React from 'react';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/lib/integration/react';
+import { YellowBox, View } from 'react-native';
 import { persistor, store } from './store';
 import { RootComponent, LoadingView } from './components';
+
+if (__DEV__) {
+  YellowBox.ignoreWarnings(['']);
+  console.disableYellowBox = true;
+}
 
 const App = () => {
   return (
